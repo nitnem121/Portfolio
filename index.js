31ce2df849f828f825c79f3ccb787bb9c24fd52f
nitnem121 <116264315+nitnem121@users.noreply.github.com>
2023-08-05 11:52:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ var pass = process.env.AUTH_PASSWORD;
 app.set("port", port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname , 'public/index.html')));
+    res.sendFile(path.join(__dirname , 'public/index.html'));
 });
 app.get("/send-email", function(req,res){
     alert("Message Sent!");

10e758b765138987b77e71b5f6a0747e1750510c
nitnem121 <116264315+nitnem121@users.noreply.github.com>
2023-08-05 11:49:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname , 'public/index.html'));
+    res.sendFile(path.join(__dirname , 'public/index.html')));
 });
 app.get("/send-email", function(req,res){
     alert("Message Sent!");
